34ff66bbaa92f2a0f33e9b673e86aa423b9ffb2a
Priizy12 <ivailsonqueiroz9@gmail.com>
2023-12-13 17:05:55 -0300

    adicionando instruções para api

---
+++ README.md
@@ -0,0 +1,8 @@
+## Baixar os pacotes do sistema
+     npm install
+## Rodar o sistema de forma local
+    npm run dev  |  yarn dev
+
+## Gerar uma nova migration 
+    yarn prisma migrate dev | npm run prisma migrate dev
+
--- src/controllers/Candidatos/create.ts
+++ src/controllers/Candidatos/create.ts
@@ -1,19 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+import { Images, PrismaClient } from "@prisma/client";
 import { RequestHandler, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 import { validation } from "../../middlewares/Validation";
 import { ICandidato } from "../../database/models/candidatos";
-import axios from 'axios'
 const prisma = new PrismaClient();
 
-interface IBodyProps extends Omit<ICandidato, "id_candidato" > {}
+interface IBodyProps extends Omit<ICandidato, "id_candidato"> { }
 
 export const candidatoUpValidation = validation((getSchema) => ({
 	body: getSchema<IBodyProps>(
 		yup.object().shape({
 			name: yup.string().required().min(5),
-            apelido: yup.string().optional().max(60),
+			apelido: yup.string().optional().max(60),
 			Partido: yup.string().optional(),
 			cidade: yup.string().required(),
 			estado: yup.string().required()
@@ -22,53 +21,30 @@ export const candidatoUpValidation = validation((getSchema) => ({
 }))
 
 
-/*const getEnderecoByCep = async (cep: string) => {
-	try {
-	  const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-	  return response.data;
-	} catch (error: any) {
-	  console.error('Erro ao obter endereço por CEP:', error.message);
-	  throw error;
-	}
-  };*/
-
 export const create = async (req: Request<{}, {}, ICandidato>, res: Response) => {
-	const { name, apelido, Partido, cidade, estado} = req.body;
+	const { name, apelido, Partido, cidade, estado } = req.body;
 	const requestImages = req.files as Express.Multer.File[];
 	try {
-		/*const endereco = await getEnderecoByCep(cep);
-		if (!endereco || endereco.erro) {
-			return res.status(StatusCodes.BAD_REQUEST).json({
-			  error: {
-				msg: "CEP inválido ou não encontrado.",
-			  },
-			});
-		  }*/
-
-		  let images: any[] = [];
-		  if (requestImages && Array.isArray(requestImages)) {
-			  images = requestImages.map((image) => {
-				  return {
-					  Url: image.filename,
-				  };
-			  });
-		  }
 		
+		const images = requestImages.map((image) => {
+			return {
+				Url: image.filename,
+			};
+		});
 
 		const candidato = await prisma.candidato.create({
 			data: {
 				name,
-                apelido,
+				apelido,
 				Partido,
 				cidade,
 				estado,
-				images:{
+				images: {
 					create: images
 				}
 			}
 		});
 
-		
 		return res.status(StatusCodes.OK).json({
 			msg: "Candidato Registrado com sucesso",
 			candidato,
